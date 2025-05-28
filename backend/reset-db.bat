ae2cb69438d5d2b843863d372faecf5c0e879d52
cchrCoding05 <194126952+cchrCoding05@users.noreply.github.com>
2025-05-28 15:18:32 +0200

    Vaciar rama deploy

--- backend/reset-db.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Reiniciando la base de datos y cargando fixtures...
-php bin/console app:reset-database
-pause 
--- backend/start-server.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Iniciando servidor Symfony desde %cd%...
-symfony server:start --no-tls
-pause 
